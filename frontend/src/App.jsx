bba00e3e288ac1c71ca652cf221a85618c9ae0a4
Naman Sharma <namansharma50962@gmail.com>
2024-04-28 20:55:37 +0530

    adding styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,44 @@ function App() {
         
         <button>Add new transaction</button>
       </form>
+
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className="left">
+            <div className="name">Iphone</div>
+            <div className='description'>It was time for an Iphone</div>
+          </div>
+
+          <div className="right">
+            <div className="price red"> - RS 80,000</div>
+            <div className="datetime">2024-4-23 16:30</div>
+          </div>
+        </div>
+
+        <div className='transaction'>
+          <div className="left">
+            <div className="name">New samsung TV</div>
+            <div className='description'>It was time for new TV</div>
+          </div>
+
+          <div className="right">
+            <div className="price red"> - RS 20,000</div>
+            <div className="datetime">2024-2-23 16:30</div>
+          </div>
+        </div>
+
+        <div className='transaction'>
+          <div className="left">
+            <div className="name">Website development</div>
+            <div className='description'>Developed a new website for a local business</div>
+          </div>
+
+          <div className="right">
+            <div className="price green"> + RS 60,000</div>
+            <div className="datetime">2024-2-23 16:30</div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
